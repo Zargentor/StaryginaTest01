675d1eb1d701a212efe8ce9c74c83cf71f4e6356
Mattew <Zarger89@gmail.com>
2025-04-17 19:38:15 +0400

    Размышление о вечном... Добавил обработку по задаче, теперь умеем находить пары

--- StaryginaTest01/ArrayProcessor.cs
+++ StaryginaTest01/ArrayProcessor.cs
@@ -8,5 +8,20 @@ namespace StaryginaTest01
 {
     internal class ArrayProcessor
     {
+        public (int index1, int value1, int index2, int value2)[] FindPairs(int[] array)
+        {
+            var pairs = new List<(int index1, int value1, int index2, int value2)>();
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] % 3 == 0 && array[i + 1] < 0)
+                {
+                    pairs.Add((i, array[i], i + 1, array[i + 1]));
+                }
+            }
+
+            return pairs.ToArray();
+        }
     }
+
 }
